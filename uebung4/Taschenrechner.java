605c7fceb90b404029466f4df9a011a7fb4b557f
KenTacki <KenTacki@users.noreply.github.com>
2016-11-01 22:49:49 +0100

    calculator main and methods

---
+++ uebung4/Taschenrechner.java
@@ -0,0 +1,37 @@
+public class Taschenrechner {
+
+	public static void main(String[] args) {
+		double ergebnis = 0.0;
+		if (args[1].equals("add"))
+			ergebnis = add(Double.parseDouble(args[0]), Double.parseDouble(args[2]));
+		if (args[1].equals("sub"))
+			ergebnis = sub(Double.parseDouble(args[0]), Double.parseDouble(args[2]));
+		if (args[1].equals("mul"))
+			ergebnis = mul(Double.parseDouble(args[0]), Double.parseDouble(args[2]));
+		if (args[1].equals("div"))
+			ergebnis = div(Double.parseDouble(args[0]), Double.parseDouble(args[2]));
+
+		System.out.println(args[0] + " " + args[1] + " " + args[2] + " ist: " + ergebnis);
+	}
+
+	public static double add(double a, double b){
+		double erg = a + b;
+		return erg;
+	}
+
+	public static double sub(double a, double b){
+		double erg = a - b;
+		return erg;
+	}
+
+	public static double mul(double a, double b){
+		double erg = a * b;
+		return erg;
+	}
+
+	public static double div(double a, double b){
+		double erg = a / b;
+		return erg;
+	}
+
+}
